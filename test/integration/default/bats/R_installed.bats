3e035805848a0d88e302e56d3860ea43d4373108
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-11-09 16:25:09 +0900

    update R version 3.2.2

--- test/integration/default/bats/R_installed.bats
+++ test/integration/default/bats/R_installed.bats
@@ -1,5 +1,5 @@
 #!/usr/bin/env bats
 @test "R development version binary is found in PATH" {
-    run ls /usr/local/R/3.1.2/bin/R
+    run ls /usr/local/R/3.2.2/bin/R
     [ "$status" -eq 0 ]
 }
--- test/integration/default/minitest/test_default.rb
+++ test/integration/default/minitest/test_default.rb
@@ -4,13 +4,13 @@ Minitest::Reporters.use! [Minitest::Reporters::JUnitReporter.new(reports_dir="/t
 
 describe 'check R version' do
   it "check R version" do
-    system('/usr/local/R/3.1.2/bin/R CMD BATCH showversion.R')
-    assert system('grep "R version 3.1.2 (2014-10-31) -- \"Pumpkin Helmet\"" showversion.Rout'), 'R version is not expected version. maybe r-base package is updated'
+    system('/usr/local/R/3.2.2/bin/R CMD BATCH showversion.R')
+    assert system('grep "R version 3.2.2 (2014-10-31) -- \"Pumpkin Helmet\"" showversion.Rout'), 'R version is not expected version. maybe r-base package is updated'
   end
 end
 describe 'check Bioconductor version' do
   it "check Bioconductor version" do
-    system('/usr/local/R/3.1.2/bin/R CMD BATCH showBioconductorVersion.R')
+    system('/usr/local/R/3.2.2/bin/R CMD BATCH showBioconductorVersion.R')
     # develop
     #assert system('grep "Bioconductor version 3.0 (BiocInstaller 1.15.3)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
     # stable

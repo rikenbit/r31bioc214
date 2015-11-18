826bc4e54245075c292324de4382e045d2009359
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-11-18 13:28:21 +0900

    Use R-patched source 3.2.2
    I can not pass 'make check' with R-3.2.2 source code.

--- test/integration/default/minitest/test_default.rb
+++ test/integration/default/minitest/test_default.rb
@@ -5,7 +5,8 @@ Minitest::Reporters.use! [Minitest::Reporters::JUnitReporter.new(reports_dir="/t
 describe 'check R version' do
   it "check R version" do
     system('/usr/local/R/3.2.2/bin/R CMD BATCH showversion.R')
-    assert system('grep "R version 3.2.2 (2014-10-31) -- \"Pumpkin Helmet\"" showversion.Rout'), 'R version is not expected version. maybe r-base package is updated'
+    #assert system('grep "R version 3.2.2 (2014-10-31) -- \"Pumpkin Helmet\"" showversion.Rout'), 'R version is not expected version. maybe r-base package is updated'
+    assert system('grep "R version 3.2.2" showversion.Rout'), 'R version is not expected version. maybe r-base package is updated'
   end
 end
 describe 'check Bioconductor version' do
@@ -14,6 +15,7 @@ describe 'check Bioconductor version' do
     # develop
     #assert system('grep "Bioconductor version 3.0 (BiocInstaller 1.15.3)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
     # stable
-    assert system('grep "Bioconductor version 3.0 (BiocInstaller 1.16.5)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
+    #assert system('grep "Bioconductor version 3.0 (BiocInstaller 1.16.5)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
+    assert system('grep "Bioconductor version 3.2 (BiocInstaller 1.20.0)" showBioconductorVersion.Rout'), 'Bioconductor version is not expected version. maybe Bioconductor package is updated'
   end
 end
